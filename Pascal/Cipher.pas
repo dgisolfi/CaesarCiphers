eedca8a644a0607cef3f1326e8f9d88ed12b259f
dgisolfi <daniel.gisolfi1@marist.edu>
2018-09-03 20:27:21 -0400

    Infinite loop fixed, started spliting string to and array

--- Pascal/Cipher.pas
+++ Pascal/Cipher.pas
@@ -16,58 +16,72 @@ begin
     writeln('> encrypt');
     writeln('> decrypt');
     writeln('> solve[break]');
-    writeln('> quit');
+    writeln('> exit');
 end;
 
-procedure encrypt;
+procedure encrypt(var offset: Integer);
+var
+  Str: string;
+  Arr: array of Char;
+  i: Integer;
+  Len: Integer;
 begin
     ClrScr;
+    write('Please input a string to be encrypted => ');
+    readln(Str);
+    Len := Length(Str);
+    SetLength(arr, Len);
+    for i := 1 to Len do
+        Arr[i - 1] := Str[i];
+    writeln(offset);
 
 end;
 
 procedure decrypt;
 begin
     ClrScr;
-
 end;
 
 procedure solve;
 begin
     ClrScr;
-
-end;
-
-procedure quit;
-begin
-    ClrScr;
-    writeln('Goodbye');
-    Halt (1);
 end;
 
 procedure main;
 var action: string;
 begin
-    ClrScr;
-    writeln('Run "help" for a list of possible commands');
-    write('What would you like to do => ');
-    readln(action);
-    if (action = 'help') then
-        help;
-        main;
-    // else if (action = 'encrypt') then
-    //     encrypt;
-    //     main;
-    // if (action = 'decrypt') then
-    //     decrypt;
-    //     main;
-    // if (action = 'solve') then
-    //     solve;
-    //     main;
-    // if (action = 'quit') then
-    //     quit;
+    offset := 6;
+    repeat
+        ClrScr;
+        writeln('Run "help" for a list of possible commands');
+        write('What would you like to do => ');
+        readln(action);
+        if (action = 'help') then
+            begin
+                help;
+                readln;
+            end;
+        if (action = 'encrypt') then
+            begin
+                encrypt(offset);
+                readln;
+            end;
+        if (action = 'decrypt') then
+            begin
+                decrypt;
+                readln;
+            end;
+        if (action = 'solve') then
+            begin
+                decrypt;
+                readln;
+            end;
+    until (action = 'exit');
 end;
 
-begin
+var
+    offset: Integer;
+begin 
     ClrScr;
     intro;
     main;
