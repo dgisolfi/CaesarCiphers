a25bcfe4ae3efbee5195dd98fc57fa94e45dc8eb
dgisolfi <daniel.gisolfi1@marist.edu>
2018-09-26 19:34:29 -0400

    Fortran Cipher Complete

--- Fortran/Cipher.f90
+++ Fortran/Cipher.f90
@@ -1,57 +1,80 @@
 program Cipher
-    ! Defualt Value
-    integer, parameter :: offset = 10
-    character(*), parameter :: str = ""
-    ! WRITE(*,*) 'Input a string =>' 
-    read(*), str
-    PRINT *, str
+  implicit none
+  ! define a defualt value for the key
+  integer :: offset = 10    
 
+  ! Input string to be encrypted 
+  character(3) :: str = 'DAN'
+  character(3) :: og = ''
+  character(3) :: encrypted = ''
+  og = str
 
-!   character(43) :: message = "The five boxing wizards jump quickly"
- 
+  write(*, '(2a)') 'Original string  = '//og
+  
+  ! Call the encrypt and concat using `//` <= why would they choose that!
+  call encrypt(str, offset)
+  write(*, '(2a)') 'Encrypt: '//og//' => '//str
+  ! Store the encrypted value
+  encrypted = str
+  ! Repeat the proccess for decryption 
+  call decrypt(str, offset)
+  write(*, '(2a)') 'Decrypted: '//encrypted//' => '//str
 
-
-
-!   write(*, "(2a)") "Original message  = ", message
-!   call encrypt(message)
-!   write(*, "(2a)") "Encrypted message = ", message
-!   call decrypt(message)
-!   write(*, "(2a)") "Decrypted message = ", message
+  ! encrypt for [0,26]
+  call solve(str, og)
+  
  
 contains
  
-subroutine encrypt(str)
-  character(*), intent(inout) :: str
+subroutine encrypt(text, shift)
+  character(*), intent(inout) :: text
+  integer :: shift
   integer :: i
- 
-  do i = 1, len(str)
-    select case(str(i:i))
+
+  ! do loop, iterate through the string
+  do i = 1, len(text)
+    select case(text(i:i))
       case ('A':'Z')
-        str(i:i) = achar(modulo(iachar(str(i:i)) - 65 + offset, 26) + 65)
+        ! use ascii code and add the shift....thank you stack overflow
+        text(i:i) = achar(modulo(iachar(text(i:i)) - 65 + shift, 26) + 65)
       case ('a':'z')
-        str(i:i) = achar(modulo(iachar(str(i:i)) - 97 + offset, 26) + 97)
+        ! do the same for lowercase leters
+        text(i:i) = achar(modulo(iachar(text(i:i)) - 97 + shift, 26) + 97)
     end select
   end do
 end subroutine
  
-subroutine decrypt(str)
-  character(*), intent(inout) :: str
+subroutine decrypt(text, shift)
+  character(*), intent(inout) :: text
+  integer :: shift
   integer :: i
  
-  do i = 1, len(str)
-    select case(str(i:i))
+  do i = 1, len(text)
+    select case(text(i:i))
       case ('A':'Z')
-        str(i:i) = achar(modulo(iachar(str(i:i)) - 65 - offset, 26) + 65)
+        ! subtract the same shift interval
+        text(i:i) = achar(modulo(iachar(text(i:i)) - 65 - shift, 26) + 65)
       case ('a':'z')
-        str(i:i) = achar(modulo(iachar(str(i:i)) - 97 - offset, 26) + 97)
+        ! do the same for the lil letters
+        text(i:i) = achar(modulo(iachar(text(i:i)) - 97 - shift, 26) + 97)
     end select
   end do
 end subroutine
 
-subroutine solve(str)
-  character(*), intent(inout) :: str
+subroutine solve(text, og)
+  character(*), intent(inout) :: text
+  character(*), intent(inout) :: og
+  character(4) :: key
   integer :: i
-
+  
+  ! The easy part.....call encrypt 27 times
+  do i = 0, 26
+    call encrypt(text, i)
+    ! convert int to string.....I miss .lower()
+    write(key, '(I2)') i 
+    ! trim the key because it gets weird otherwise
+    write(*, '(2a)') 'Key:'//trim(key)//' => '//text
+  end do
 end subroutine
  
 end program Cipher
--- Fortran/Log.md
+++ Fortran/Log.md
@@ -3,6 +3,6 @@
 |    Date    | Hours | Tasks / Accomplishments / Issues / Thoughts                  |
 | :--------: | :---: | :----------------------------------------------------------- |
 | 2018-09-20 |   2   | Wrote a Encrypt and Decrypt Subroutine...The logic and syntax of encrypting is simmilar to pascal. The syntax is awful compared to pascal. I miss Pascal :( |
-|            |       |                                                              |
+| 2018-09-26 |   3   | Once alan enlightened me on the wonders of page two of the assignment this became easier....Calling the encrypt function with a for loop isn't too hard. But who the hell chose // as the concatenation character. |
 
 To run the program use `./Cipher` in the same directory as the Cipher file
