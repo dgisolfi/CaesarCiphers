6ee0315508a4960d120b6d7e8075514193bf8bbb
dgisolfi <daniel.gisolfi1@marist.edu>
2018-11-27 10:45:02 -0500

    Decrypt Function works

--- Erlang/ceasar.erl
+++ Erlang/ceasar.erl
@@ -1,5 +1,5 @@
 -module(ceasar).
--export([main/0, offset/2, key/1, encrypt/2, decrypt/2, solve/3]).
+-export([main/0, offset/2, encrypt/2, decrypt/2]).
 
 offset(Char,Key) when (Char >= $A) and (Char =< $Z) or
                    (Char >= $a) and (Char =< $z) ->
@@ -9,43 +9,20 @@ offset(Char,Key) when (Char >= $A) and (Char =< $Z) or
 offset(Char, _Key) ->
   Char.
  
-% key: normalize key.
-key(Key) when Key < 0 ->
-  26 + Key rem 26;
-key(Key) when Key > 25 ->
-  Key rem 26;
-key(Key) ->
-  Key.
-
 encrypt(Str, Key) ->
-  [lists:map(fun(Char) -> offset(Char, Key) end, Str)].
+  lists:map(fun(Char) -> offset(Char, Key) end, Str).
 
 decrypt(Str, Key) ->
-  % Key = key(-k),
-  encrypt(Str, -Key).
-
-solve(Str, Cur, Lim) ->
-  c = cur + 1,
-  encrypted = encrypt(Str, Cur),
-  io:format("Ceasar :~s~n", [encrypted]),
-  if 
-    cur == Lim -> 
-      io:fwrite("Done"); 
-    true -> 
-      solve(Str, c, Lim);
-  end.
-
+  encrypt(Str, Key).
  
 main() ->
   OG = "HAL",
   Key = 6,
-  % Encode = key(Key),
-  % Decode = key(-Key),
 
   Encrypted = encrypt(OG, Key),
-  Decrypted = decrypt(Encrypted, Key),
+  Decrypted = decrypt(Encrypted, -Key),
 
   io:format("Original  ---> ~s~n", [OG]),
   io:format("Encrypted ---> ~s~n", [Encrypted]),
-  io:format("Decrypted ---> ~s~n", [Decrypted]),
-  solve(OG, 0, 26).
+  io:format("Decrypted ---> ~s~n", [Decrypted]).
+  % solve(OG, 0, 26).
